45894fb164043d92e10d97670cb751a33538c617
adsingh1 <adsingh327@yahoo.com>
2021-06-09 21:46:59 -0500

    from raghu github

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
+
 source components/common.sh
 rm -f /tmp/roboshop.log
+set-hostname frontend
 
-HEAD "Start nginx\t"
+HEAD "Installing Nginx\t"
 yum install nginx -y &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Download the HTDOC"
+HEAD "Download from GitHub\t"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Delete old html DOCS\t"
+HEAD "Delete old HTML DOCS\t"
 rm -rf /usr/share/nginx/html/*
 STAT $?
 
-HEAD "Extract & Deploy contents"
+HEAD "Extract Downloaded Content"
 unzip -d /usr/share/nginx/html /tmp/frontend.zip &>>/tmp/roboshop.log
-mv /usr/share/nginx/frontend-main/* /usr/share/nginx/html/. &>>/tmp/roboshop.log
+mv /usr/share/nginx/html/frontend-main/* /usr/share/nginx/html/. &>>/tmp/roboshop.log
 mv /usr/share/nginx/html/static/* /usr/share/nginx/html/. &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Update nginx configuration"
-mv /usr/share/nginx/localhost.conf /etc/nginx/default.d/roboshop.conf
-STAT $?
-
-HEAD "start nginx\t\t"
-systemctl enable nginx &>>/tmp/roboshop.log && systemctl restart nginx &>>/tmp/roboshop.log
+HEAD "Update Nginx Configuration"
+mv /usr/share/nginx/html/localhost.conf /etc/nginx/default.d/roboshop.conf
 STAT $?
 
