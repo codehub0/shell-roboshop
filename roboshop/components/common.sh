02238e4e45a45351285616297267fb4a691e0c27
adsingh1 <adsingh327@yahoo.com>
2021-06-09 21:58:22 -0500

    1common commit

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+
 HEAD() {
-  echo -n -e "\e[1m $1 \e[0m \t\t"
+  echo -n -e "\e[1m $1 \e[0m \t\t ... "
 }
 
 STAT() {
- if [$1 -eq 0]; then
- echo -e "\e[1;32m done\e[0m"
- else
- echo -e "\e[1;31 fail\e[0m"
- echo -e "\t \e[1;33m check the log for more details ... logfile: /tmp/roboshop.log[0m"
- exit 1
- fi
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[1;32m done\e[0m"
+  else
+    echo -e "\e[1;31m fail\e[0m"
+    echo -e "\e[1;33m Check the log for more detail ... Log-File : /tmp/roboshop.log\e[0m"
+    exit 1
+  fi
 }
