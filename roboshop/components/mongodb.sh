aa12de2eff253f053282325861c050bbb12644c5
adsingh1 <adsingh327@yahoo.com>
2021-06-10 10:02:02 -0500

    again commit

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 source components/common.sh
 rm -f /tmp/roboshop.log
+set-hostname mongodb
+
+HEAD "Installing Mongodb"
+HEAD "Setup MongoDB Yum repo file\t"
+echo '[mongodb-org-4.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+STAT $?
+
