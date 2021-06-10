1c45ba29714cc5bdba68664cbede3e08ed7bb2c6
adsingh1 <adsingh327@yahoo.com>
2021-06-10 10:19:45 -0500

    again commit

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -4,7 +4,7 @@ rm -f /tmp/roboshop.log
 set-hostname mongodb
 
 HEAD "Installing Mongodb"
-HEAD "Setup MongoDB Yum repo file\t"
+HEAD "Setup MongoDB Yum repo file"
 echo '[mongodb-org-4.2]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
@@ -17,3 +17,11 @@ HEAD "Install MongoDB\t\t\t"
 yum install -y mongodb-org &>>/tmp/roboshop.log
 STAT $?
 
+HEAD "Update Listen Address in Config file"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+STAT $?
+
+HEAD "Start MongoDB Service\t\t"
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl restart mongod &>>/tmp/roboshop.log
+STAT $?
