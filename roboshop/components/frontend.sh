e85cb24292e50aab7e1eda199a1383d58611ae73
adsingh1 <adsingh327@yahoo.com>
2021-06-09 22:58:03 -0500

    again commit

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -7,3 +7,21 @@ HEAD "Installing Nginx\t"
 yum install nginx -y &>>/tmp/roboshop.log
 STAT $?
 
+HEAD "Download from GitHub\t"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Delete old HTML DOCS\t"
+rm -rf /usr/share/nginx/html/*
+STAT $?
+
+HEAD "Extract Downloaded Content"
+unzip -d /usr/share/nginx/html /tmp/frontend.zip &>>/tmp/roboshop.log
+mv /usr/share/nginx/html/frontend-main/* /usr/share/nginx/html/. &>>/tmp/roboshop.log
+mv /usr/share/nginx/html/static/* /usr/share/nginx/html/. &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Update Nginx Configuration"
+mv /usr/share/nginx/html/localhost.conf /etc/nginx/default.d/roboshop.conf
+STAT $?
+
